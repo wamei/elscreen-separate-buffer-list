1ae25f653ae12004fcdb58e56653ab25f29714be
Kazuaki Hamada <wamei.cho@gmail.com>
2015-02-19 12:12:37 +0900

    Use prefix 'esbl'

--- elscreen-separate-buffer-list.el
+++ elscreen-separate-buffer-list.el
@@ -34,171 +34,174 @@
 (eval-when-compile (require 'cl))
 (require 'elscreen)
 
-(defvar elscreen-separate-buffer-list-default '("*scratch*" "*Messages*"))
-(defvar elscreen-separate-buffer-list '())
-(defvar elscreen-separate-buffer-count-list '())
+(defvar esbl-separate-buffer-list-default '("*scratch*" "*Messages*"))
+(defvar esbl-separate-buffer-list '())
+(defvar esbl-separate-buffer-count-list '())
 
-(defun elscreen-get-separate-buffer-list (screen)
+(defun esbl-get-separate-buffer-list (screen)
   "SCREENに保存されているSEPARATE-BUFFER-LISTを取得する."
   (let ((screen-property (elscreen-get-screen-property screen)))
     (assoc-default 'separate-buffer-list screen-property)))
 
-(defun elscreen-set-separate-buffer-list (screen buflist)
+(defun esbl-set-separate-buffer-list (screen buflist)
   "SCREENにBUFLISTで与えられるSEPARATE-BUFFER-LISTを格納する."
   (let ((screen-property (elscreen-get-screen-property screen)))
     (elscreen--set-alist 'screen-property 'separate-buffer-list buflist)
     (elscreen-set-screen-property screen screen-property)))
 
-(defun elscreen-get-separate-window-history (screen)
+(defun esbl-get-separate-window-history (screen)
   "SCREENに保存されているWINDOW-HISTORYを取得する."
   (let ((screen-property (elscreen-get-screen-property screen)))
     (assoc-default 'separate-window-history screen-property)))
 
-(defun elscreen-set-separate-window-history (screen winHistory)
+(defun esbl-set-separate-window-history (screen winHistory)
   "SCREENにWINHISTORYで与えられるWINDOW-HISTORYを格納する."
   (let ((screen-property (elscreen-get-screen-property screen)))
     (elscreen--set-alist 'screen-property 'separate-window-history winHistory)
     (elscreen-set-screen-property screen screen-property)))
 
-(defun elscreen-save-separate-buffer-list (&optional screen)
+(defun esbl-save-separate-buffer-list (&optional screen)
   "SCREENに現在のSEPARATE-BUFFER-LISTを保存する."
   (let ((screen (or screen (elscreen-get-current-screen))))
-    (elscreen-set-separate-buffer-list screen elscreen-separate-buffer-list)))
+    (esbl-set-separate-buffer-list screen esbl-separate-buffer-list)))
 
-(defun elscreen-restore-separate-buffer-list (&optional screen)
+(defun esbl-restore-separate-buffer-list (&optional screen)
   "SCREENに保存されているSEPARATE-BUFFER-LISTを復元する."
   (let* ((screen (or screen (elscreen-get-current-screen)))
-         (buffList (elscreen-get-separate-buffer-list screen)))
+         (buffList (esbl-get-separate-buffer-list screen)))
     (if buffList
-        (setq elscreen-separate-buffer-list buffList)
-      (setq elscreen-separate-buffer-list (elscreen-make-default-separate-buffer-list)))))
+        (setq esbl-separate-buffer-list buffList)
+      (setq esbl-separate-buffer-list (esbl-make-default-separate-buffer-list)))))
 
-(defun elscreen-add-separate-buffer-list (buffer)
+(defun esbl-add-separate-buffer-list (buffer)
   "SEPARATE-BUFFER-LISTにBUFFERを加える."
-  (unless (member buffer elscreen-separate-buffer-list)
-    (setq elscreen-separate-buffer-list (append (list buffer) elscreen-separate-buffer-list))
-    (elscreen-separate-buffer-list-count-inc buffer)))
+  (unless (member buffer esbl-separate-buffer-list)
+    (setq esbl-separate-buffer-list (append (list buffer) esbl-separate-buffer-list))
+    (esbl-separate-buffer-list-count-inc buffer)))
 
-(defun elscreen-remove-separate-buffer-list (buffer)
+(defun esbl-remove-separate-buffer-list (buffer)
   "SEPARATE-BUFFER-LISTからBUFFERを取り除く."
-  (elscreen-separate-buffer-list-count-dec buffer)
-  (setq elscreen-separate-buffer-list (loop for i in elscreen-separate-buffer-list
+  (esbl-separate-buffer-list-count-dec buffer)
+  (setq esbl-separate-buffer-list (loop for i in esbl-separate-buffer-list
                                             unless (equal i buffer)
                                             collect i)))
 
-(defun elscreen-update-separate-buffer-list ()
+(defun esbl-update-separate-buffer-list ()
   "SEPARATE-BUFFER-LISTを更新する."
-  (elscreen-separate-buffer-list-count-clean)
-  (setq elscreen-separate-buffer-list (loop for i in elscreen-separate-buffer-list
+  (esbl-separate-buffer-list-count-clean)
+  (setq esbl-separate-buffer-list (loop for i in esbl-separate-buffer-list
                                             if (buffer-live-p i)
                                             collect i)))
 
-(defun elscreen-separate-buffer-list-count-inc (buffer)
+(defun esbl-separate-buffer-list-count-inc (buffer)
   "BUFFERのカウントを上げる."
-  (loop for i in elscreen-separate-buffer-count-list
+  (loop for i in esbl-separate-buffer-count-list
            if (equal (car i) buffer)
            do (setcdr i  (+ 1 (cdr i)))
            and return nil
-           finally (push (cons buffer 1) elscreen-separate-buffer-count-list)))
+           finally (push (cons buffer 1) esbl-separate-buffer-count-list)))
 
-(defun elscreen-separate-buffer-list-count-dec (buffer)
+(defun esbl-separate-buffer-list-count-dec (buffer)
   "BUFFERのカウントを下げる."
-  (setq elscreen-separate-buffer-count-list   (loop for i in elscreen-separate-buffer-count-list
+  (setq esbl-separate-buffer-count-list   (loop for i in esbl-separate-buffer-count-list
                                                        if (equal (car i) buffer)
                                                        do (setcdr i  (- (cdr i) 1))
                                                        if (< 0 (cdr i))
                                                        collect i)))
 
-(defun elscreen-separate-buffer-list-count (buffer)
+(defun esbl-separate-buffer-list-count (buffer)
   "BUFFERのカウントを返す."
-  (loop for i in elscreen-separate-buffer-count-list
+  (loop for i in esbl-separate-buffer-count-list
            if (equal (car i) buffer)
            return (cdr i)
            finally return 0))
 
-(defun elscreen-separate-buffer-list-count-clean ()
+(defun esbl-separate-buffer-list-count-clean ()
   "BUFFER-COUNTの掃除をする."
-  (setq elscreen-separate-buffer-count-list   (loop for i in elscreen-separate-buffer-count-list
+  (setq esbl-separate-buffer-count-list   (loop for i in esbl-separate-buffer-count-list
                                                        if (buffer-live-p (car i))
                                                        collect i)))
 
-(defun elscreen-goto:restore-separate-buffer-list (origin &rest args)
+(defun esbl-goto:elscreen-goto (origin &rest args)
   "SCREENの切替時にSEPARATE-BUFFER-LIST,WINDOW-HISTORYを復元する."
-  (elscreen-set-separate-window-history (elscreen-get-current-screen) (elscreen-get-all-window-history-alist))
+  (esbl-set-separate-window-history (elscreen-get-current-screen) (esbl-get-all-window-history-alist))
   (apply origin args)
-  (elscreen-restore-all-window-history-alist (elscreen-get-separate-window-history (elscreen-get-current-screen)))
+  (esbl-restore-all-window-history-alist (esbl-get-separate-window-history (elscreen-get-current-screen)))
   (when (elscreen-screen-live-p (elscreen-get-previous-screen))
-    (elscreen-save-separate-buffer-list (elscreen-get-previous-screen)))
-  (elscreen-restore-separate-buffer-list (elscreen-get-current-screen)))
-(advice-add 'elscreen-goto :around 'elscreen-goto:restore-separate-buffer-list)
+    (esbl-save-separate-buffer-list (elscreen-get-previous-screen)))
+  (esbl-restore-separate-buffer-list (elscreen-get-current-screen)))
+(advice-add 'elscreen-goto :around 'esbl-goto:elscreen-goto)
 
-(defun elscreen-clone:clone-separate-buffer-list (&rest _)
+(defun esbl-clone:elscreen-clone (&rest _)
   "SCREENの複製時にSEPARATE-BUFFER-LISTも複製する."
-  (let ((stack (elscreen-get-separate-buffer-list (elscreen-get-previous-screen))))
-    (elscreen-set-separate-buffer-list (elscreen-get-current-screen) stack)
-    (elscreen-restore-separate-buffer-list (elscreen-get-current-screen))))
-(advice-add 'elscreen-clone :after 'elscreen-clone:clone-separate-buffer-list)
+  (let ((stack (esbl-get-separate-buffer-list (elscreen-get-previous-screen))))
+    (esbl-set-separate-buffer-list (elscreen-get-current-screen) stack)
+    (esbl-restore-separate-buffer-list (elscreen-get-current-screen))))
+(advice-add 'elscreen-clone :after 'esbl-clone:elscreen-clone)
 
-(defun elscreen-kill:restore-separate-buffer-list (origin &rest args)
+(defun esbl-kill:elscreen-kill (origin &rest args)
   "SCREENの削除時にBUFFERの削除、SEPARATE-BUFFER-LISTの復元をする."
   (mapc (lambda (buffer)
-          (unless (member (buffer-name buffer) elscreen-separate-buffer-list-default)
+          (unless (member (buffer-name buffer) esbl-separate-buffer-list-default)
             (kill-buffer buffer)))
         (buffer-list))
   (apply origin args)
-  (elscreen-restore-separate-buffer-list (elscreen-get-current-screen)))
-(advice-add 'elscreen-kill :around 'elscreen-kill:restore-separate-buffer-list)
+  (esbl-restore-separate-buffer-list (elscreen-get-current-screen)))
+(advice-add 'elscreen-kill :around 'esbl-kill:elscreen-kill)
 
-(defun elscreen-separate-buffer-list-kill-buffer-hook ()
+(defun esbl-kill-buffer-hook ()
   "BUFFER削除時にSEPARATE-BUFFER-LISTからも削除する."
   (let ((buffer (current-buffer)))
-    (when (member buffer elscreen-separate-buffer-list)
-      (elscreen-remove-separate-buffer-list buffer)
-      (if (> 1 (elscreen-separate-buffer-list-count buffer))
+    (when (member buffer esbl-separate-buffer-list)
+      (esbl-remove-separate-buffer-list buffer)
+      (if (> 1 (esbl-separate-buffer-list-count buffer))
           t
         (bury-buffer)
         nil))))
-(add-hook 'kill-buffer-query-functions 'elscreen-separate-buffer-list-kill-buffer-hook)
+(add-hook 'kill-buffer-query-functions 'esbl-kill-buffer-hook)
 
-(defun elscreen-separate-buffer-list-buffer-list-update-hook ()
+(defun esbl-buffer-list-update-hook ()
   "BUFFER-LIST更新時にSEPARATE-BUFFER-LISTも更新する."
-  (elscreen-update-separate-buffer-list))
-(add-hook 'buffer-list-update-hook 'elscreen-separate-buffer-list-buffer-list-update-hook)
+  (esbl-update-separate-buffer-list))
+(add-hook 'buffer-list-update-hook 'esbl-buffer-list-update-hook)
 
-(defun switch-to-buffer:elscreen-separate-buffer-list (buffer &rest _)
+(defun esbl-add-separate-buffer-list:switch-to-buffer (buffer &rest _)
   "BUFFER切り替え時にSEPARATE-BUFFER-LISTに追加する."
-  (elscreen-add-separate-buffer-list (get-buffer buffer)))
-(advice-add 'switch-to-buffer :after 'switch-to-buffer:elscreen-separate-buffer-list)
+  (esbl-add-separate-buffer-list (get-buffer buffer)))
+(advice-add 'switch-to-buffer :after 'esbl-add-separate-buffer-list:switch-to-buffer)
 
-(defun get-buffer-create:elscreen-separate-buffer-list (buffer &rest _)
+(defun esbl-add-separate-buffer-list:get-buffer-create (buffer &rest _)
   "GET-BUFFER-CREATE時にSEPARATE-BUFFER-LISTに追加する."
-  (elscreen-add-separate-buffer-list (get-buffer buffer)))
-(advice-add 'get-buffer-create :after 'get-buffer-create:elscreen-separate-buffer-list)
+  (esbl-add-separate-buffer-list (get-buffer buffer)))
+(advice-add 'get-buffer-create :after 'esbl-add-separate-buffer-list:get-buffer-create)
 
-(defun buffer-list:return-separate-buffer-list (origin &rest _)
+(defun esbl-return-separate-buffer-list:buffer-list (origin &rest _)
   "BUFFER-LISTが呼ばれた際にSEPARATE-BUFFER-LISTでフィルタリングを行う."
   (loop for i in (apply origin _)
-           if (member (get-buffer i) elscreen-separate-buffer-list)
+           if (member (get-buffer i) esbl-separate-buffer-list)
            collect i))
-(advice-add 'buffer-list :around 'buffer-list:return-separate-buffer-list)
+(advice-add 'buffer-list :around 'esbl-return-separate-buffer-list:buffer-list)
 
 ;; elscreenのパッチからパクってきた
-(defun elscreen-window-history-supported-p ()
+(defun esbl-window-history-supported-p ()
+  "WINDOW-HISTORYに対応しているかどうか."
   (and (fboundp 'window-prev-buffers)
        (fboundp 'window-next-buffers)
        (fboundp 'set-window-prev-buffers)
        (fboundp 'set-window-next-buffers)))
 
-(defun elscreen-get-all-window-history-alist ()
-  (when (elscreen-window-history-supported-p)
+(defun esbl-get-all-window-history-alist ()
+  "全てのウィンドウのWINDOW-HISTORYをALISTにして取得する."
+  (when (esbl-window-history-supported-p)
     (mapcar (lambda (window)
               (let ((prevs (window-prev-buffers window))
                     (nexts (window-next-buffers window)))
                 (cons window (cons prevs nexts))))
             (window-list))))
 
-(defun elscreen-restore-all-window-history-alist (history-alist)
-  (when (elscreen-window-history-supported-p)
+(defun esbl-restore-all-window-history-alist (history-alist)
+  "HISTORY-ALISTからWINDOW-HISTORYを復元する."
+  (when (esbl-window-history-supported-p)
     (mapc (lambda (entry)
             (let* ((window (car entry))
                    (histories (cdr entry))
@@ -211,15 +214,15 @@
 
 ;; 初期化
 
-(defun elscreen-make-default-separate-buffer-list ()
+(defun esbl-make-default-separate-buffer-list ()
   "デフォルトのバッファリストを作成する."
-  (loop for i in elscreen-separate-buffer-list-default
+  (loop for i in esbl-separate-buffer-list-default
            collect (get-buffer i)))
 
-(defun elscreen-separate-buffer-list-init-hook ()
+(defun esbl-init-hook ()
   "初期化関数.デフォルトのバッファリストを設定する."
-  (setq elscreen-separate-buffer-list (elscreen-make-default-separate-buffer-list)))
-(add-hook 'after-init-hook 'elscreen-separate-buffer-list-init-hook)
+  (setq esbl-separate-buffer-list (esbl-make-default-separate-buffer-list)))
+(add-hook 'after-init-hook 'esbl-init-hook)
 
 (provide 'elscreen-separate-buffer-list)
 
