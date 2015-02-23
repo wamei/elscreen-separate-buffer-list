2e3e385abd8521f762a9bc5b3acd183e3fc3c0ef
Kazuaki Hamada <wamei.cho@gmail.com>
2015-02-23 15:36:25 +0900

    Update README

--- elscreen-separate-buffer-list.el
+++ elscreen-separate-buffer-list.el
@@ -29,6 +29,7 @@
 ;;      (require 'elscreen-separate-buffer-list)
 ;;      (elscreen-separate-buffer-list-mode)
 ;;
+;; This apply to ido-mode or something that uses ido-make-buffer-list such as helm.
 
 ;;; Code:
 
